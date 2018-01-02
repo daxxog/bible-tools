fb2fd0fc28fe9e0755a2dd66a8ca5b171ac0ae92
David Volm <david@volminator.com>
2018-01-02 12:40:32 -0600

    npm module init

---
+++ xetex.js
@@ -0,0 +1,26 @@
+/* xetex.js
+ * simple parser for tex files
+ * (c) 2018 David (daXXog) Volm ><> + + + <><
+ * Released under Apache License, Version 2.0:
+ * http://www.apache.org/licenses/LICENSE-2.0.html  
+ */
+
+/* UMD LOADER: https://github.com/umdjs/umd/blob/master/returnExports.js */
+(function (root, factory) {
+    if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like enviroments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(factory);
+    } else {
+        // Browser globals (root is window)
+        root.XeTeX = factory();
+  }
+}(this, function() {
+    var XeTeX;
+    
+    return XeTeX;
+}));
